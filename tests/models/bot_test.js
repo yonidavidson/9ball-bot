708059a98544b41efa241070e8ba19d5faad9235
Yoni Davidson <yoni.davidson@eyesight-tech.com>
2016-06-09 23:39:59 +0300

    model test

--- tests/models/bot_test.js
+++ tests/models/bot_test.js
@@ -1,8 +1,15 @@
 var bot = require('../../models/bot');
 var assert = require('chai').assert;
+const challange = 'Fortuna'
 
-describe('bot validate', function(){
-  it('should return: \"err....\"', function(){
-    assert.notEqual(true,true);
+describe('bot validate fail', function(){
+  it('should not return challange', function(){
+    assert.notEqual(bot.validate('blabla',challange),challange);
+  });
+});
+
+describe('bot validate success', function(){
+  it('should not challange', function(){
+    assert.equal(bot.validate('blabla',challange),challange)
   });
 });
